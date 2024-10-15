4bf8db6223ee996ecb286cebabcfe04b794ea3d9
BrendaStreng <brendastreng@hotmail.com>
2024-10-14 21:37:58 -0300

    Add files via upload

---
+++ Projeto-Signos/js/main.js
@@ -0,0 +1,103 @@
+
+function qualSigno() {
+    var diaNasc = document.getElementById("diaNasc").value;
+    var mesNasc = document.getElementById("mesNasc").value;
+
+
+
+
+    if (mesNasc == 1) {
+        if (diaNasc <= 20) {
+            alert("Seu signo é capricornio");
+
+
+        } else {
+            alert("Seu signo é Aquario");
+        }
+    }
+    if (mesNasc == 2) {
+        if (diaNasc <= 19) {
+            alert("Seu signo é aquario");
+        } else {
+            alert("Seu signo é peixes");
+        }
+    }
+    if (mesNasc == 3) {
+        if (diaNasc <= 20) {
+            alert("Seu signo é peixes");
+
+        } else {
+            alert("Seu signo é aries");
+        }
+    }
+    if (mesNasc == 4) {
+        if (diaNasc <= 20) {
+            alert("Seu signo é aries");
+        } else {
+            alert("Seu signo é touro ");
+        }
+
+    }
+    if (mesNasc == 5) {
+        if (diaNasc <= 20) {
+            alert("Seu signo é touro");
+        } else {
+            alert("Seu signo é gemeos");
+        }
+
+    }
+    if (mesNasc == 6) {
+        if (diaNasc <= 20) {
+            alert("Seu signo é gemeos");
+        } else {
+            alert("Seu signo é cancer");
+        }
+
+    }
+    if (mesNasc == 7) {
+        if (diaNasc <= 21) {
+            alert("Seu signo é cancer");
+        } else {
+            alert("Seu signo é leão");
+        }
+    }
+    if (mesNasc == 8) {
+        if (diaNasc <= 22) {
+            alert("Seu signo é leão");
+        } else {
+            alert("seu signo é virgem");
+        }
+    }
+    if (mesNasc == 9) {
+        if (diaNasc <= 22) {
+            alert("Seu signo é virgem");
+        } else {
+            alert("Seu signo é libra");
+        }
+    }
+    if (mesNasc == 10) {
+        if (diaNasc <= 22) {
+            alert("Seu signo é libra");
+        } else {
+            alert("Seu signo é escorpião");
+        }
+    }
+    if (mesNasc == 11) {
+        if (diaNasc <= 21) {
+            alert("Seu signo é escorpião");
+        } else {
+            alert("seu signo é sagitario");
+
+        }
+    }
+    if (mesNasc == 12) {
+        if (diaNasc <= 21) {
+            alert("seu signo é sagitario");
+        } else {
+            alert("seu signo é capricornio")
+        }
+    }
+
+
+
+}
